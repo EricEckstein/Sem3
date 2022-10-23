2abbbcc11449e25cbf48041cc75de53029f51c95
eric eckstein <ai21m041@technikum-wien.at>
2022-10-23 16:38:07 +0200

    dsa

---
+++ DEVP2/CalculatorWebApp/CalculatorWebApp/CalculatorLogic/NumberOperation.cs
@@ -0,0 +1,16 @@
+﻿namespace CalculatorWebApp.CalculatorLogic
+{
+	public class NumberOperation : Operation
+	{
+		double d;
+
+		public NumberOperation(double d) : base(d,0)
+		{
+		}
+
+		public override double Eval()
+		{
+			return d;
+		}
+	}
+}
---
+++ DEVP2/CalculatorWebApp/CalculatorWebApp/CalculatorLogic/OperationSign.cs
@@ -0,0 +1,13 @@
+﻿namespace CalculatorWebApp.CalculatorLogic
+{
+	public enum OperationSign
+	{
+		Plus = 0,
+		Minus = 1,
+		Multiply = 2,
+		Divide = 3,
+		Equals = 4,
+		Clear = 5,
+		None = 6,
+	}
+}

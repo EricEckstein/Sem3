ad51fb1689ec64b1c5c492d3e5282178a1fbef5c
FHTW <ai21m041@technikum-wien.at>
2023-01-08 11:05:43 +0100

    yyyy

---
+++ AXAR/ForestFire/ForrestFire/DoStepKernel.cs
@@ -0,0 +1,39 @@
+﻿//using ILGPU;
+//using ILGPU.Runtime;
+
+//internal struct DoStepKernel
+//{
+//    public ArrayView2D<byte, Stride2D.DenseX> Input;
+//    public ArrayView2D<byte, Stride2D.DenseX> Output;
+//    public ArrayView2D<float, Stride2D.DenseX> Random;
+//    public ArrayView1D<Index2D, Stride1D.Dense> Neighbours;
+
+//    public float PGrowth;
+//    public float PFire;
+
+//    public static void Execute(Index2D idx, DoStepKernel data)
+//    {
+//        data.Output[idx] = data.Input[idx] switch
+//        {
+//            0 when data.Random[idx] < data.PGrowth => 1,
+//            0 => 0,
+
+//            1 when data.Random[idx] < data.PFire => 2,
+//            1 when data.IsNeighbourBurning(idx) => 2,
+//            1 => 1,
+
+//            _ => 0
+//        };
+//    }
+
+//    public bool IsNeighbourBurning(Index2D idx)
+//    {
+//        for(int i = 0; i < Neighbours.Length; i++) 
+//        {
+//            Index2D nIdx = idx + Neighbours[i];
+//            if(nIdx.InBounds(Input.IntExtent) && Input[idx] == 2)
+//                return true;
+//        }
+//        return false;
+//    }
+//}
---
+++ AXAR/ForestFire/ForrestFire/Program.cs
@@ -0,0 +1,79 @@
+﻿//using ILGPU;
+//using ILGPU.Runtime;
+//using ILGPU.Runtime.Cuda;
+//using System;
+
+//public class Program
+//{
+//    private DoStepKernel _doStepKernel;
+
+//    protected class Neighbourhood
+//    {
+//        int[,] buff = new int[1024, 1024];
+
+//        protected Index2D[] ToIndices()
+//        {
+//            return buff.getas
+
+//        }
+//    }
+
+//    public void DoStep(float pGrowth, float pFire, Neighbourhood neighbourhood)
+//    {
+//        _rng.FillUniform(_rndBuffer.View);
+
+//        Index2D[] nIdices = neighbourhood.ToIndices();
+//        using MemoryBuffer1D<Index2D, Stride1D.Dense> neighbourBuffer = _accelerator.Allocate1D<Index2D>(nIdices.Length);
+//        neighbourBuffer.CopyFromCPU(nIdices);
+
+//        _doStepKernel(_outputBuffer.Extent.ToIntIndex(), new DoStepKernel
+//        {
+//            Input = _inputBuffer.View,
+//            Output = _outputBuffer.View,
+//            Random = _rndBuffer.View,
+//            PGrowth = 0.2,
+//            PFire = 0.1,
+//            Neighbours = neighbourBuffer
+//        });
+
+//        (_inputBuffer, _outputBuffer) = (_outputBuffer, _inputBuffer);
+//    }
+
+static void Main()
+{ }
+//    static void Main()
+//    {
+//        // Initialize ILGPU.
+
+//        Context context = Context.Create(builder => builder.Cuda());
+//        Accelerator accelerator = context.GetPreferredDevice(preferCPU: false)
+//                                  .CreateAccelerator(context);
+
+//        // Load the data.
+//        MemoryBuffer1D<int, Stride1D.Dense> deviceData = accelerator.Allocate1D(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+//        MemoryBuffer1D<int, Stride1D.Dense> deviceOutput = accelerator.Allocate1D<int>(10_000);
+
+//        // load / precompile the kernel
+//        Action<Index1D, ArrayView<int>, ArrayView<int>> loadedKernel =
+//            accelerator.LoadAutoGroupedStreamKernel<Index1D, ArrayView<int>, ArrayView<int>>(Kernel);
+
+//        // finish compiling and tell the accelerator to start computing the kernel
+//        loadedKernel((int)deviceOutput.Length, deviceData.View, deviceOutput.View);
+
+//        // wait for the accelerator to be finished with whatever it's doing
+//        // in this case it just waits for the kernel to finish.
+//        accelerator.Synchronize();
+
+//        // moved output data from the GPU to the CPU for output to console
+//        int[] hostOutput = deviceOutput.GetAsArray1D();
+
+//        for (int i = 0; i < 50; i++)
+//        {
+//            Console.Write(hostOutput[i]);
+//            Console.Write(" ");
+//        }
+
+//        accelerator.Dispose();
+//        context.Dispose();
+//    }
+//}
